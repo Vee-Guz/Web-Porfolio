87b6ac19e9c9f305ce52ee9f37b70a27eda18afc
Vee-Guz <vero.guzman48612@gmail.com>
2022-08-27 07:13:47 +0000

    created a for loop to display photoGallery images

--- js/playground.js
+++ js/playground.js
@@ -9,19 +9,34 @@ const addElements = () => {
     {
         name: "img2",
         imgURL: "https://images.pexels.com/photos/5233320/pexels-photo-5233320.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    },
+    {
+        name: "img3",
+        imgURL: "https://images.pexels.com/photos/12705772/pexels-photo-12705772.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    },
+    {
+        name: "img4",
+        imgURL: "https://images.pexels.com/photos/12747521/pexels-photo-12747521.png?auto=compress&cs=tinysrgb&w=600&lazy=load"
     }]
 
 
+    //for every image in photoGallery
+    for (let index = 0; index < photoGallery.length; index++){
+        // create a new img element
+        let newImg = document.createElement("img");
+        // for that image element add the image url of current photo index to src
+        newImg.src = photoGallery[index]["imgURL"];
+    
+        // add that photo to the gallery
+        playground.appendChild(newImg);
+    }
+    
     let newDiv = document.createElement("div");
-
-    let newImg = document.createElement("img");
-    newImg.src = photoGallery[0]["imgURL"];
-
     const newContent = document.createTextNode("hello");
     newDiv.append(newContent);
-
     playground.appendChild(newDiv);
-    playground.appendChild(newImg);
+
+    
 }
 
 addElements();
